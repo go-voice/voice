20e3baed5bcdbec1293a6ef55a2a60752bc6d2a9
maze <maze@booking.com>
2019-02-13 00:15:52 +0100

    Added codec2 support

---
+++ nb/codec2/codec2_cgo.go
@@ -0,0 +1,221 @@
+// +build cgo
+
+package codec2
+
+/*
+#cgo CFLAGS: -I. -Wno-absolute-value
+#cgo LDFLAGS: -lm
+
+#include "codec2.h"
+typedef struct CODEC2 codec2;
+
+*/
+import "C"
+import (
+	"errors"
+	"fmt"
+	"unsafe"
+)
+
+// Mode setting
+type Mode C.int
+
+var (
+	modeName = map[Mode]string{
+		Mode3200: "3200",
+		Mode2400: "2400",
+		Mode1600: "1600",
+		Mode1400: "1400",
+		Mode1300: "1300",
+		Mode1200: "1200",
+		Mode700:  "700",
+		Mode700B: "700B",
+		Mode700C: "700C",
+	}
+	modeBitsPerFrame = map[Mode]int{
+		Mode3200: 64,
+		Mode2400: 48,
+		Mode1600: 64,
+		Mode1400: 56,
+		Mode1300: 52,
+		Mode1200: 48,
+		Mode700:  28,
+		Mode700B: 28,
+		Mode700C: 28,
+	}
+	modeSamplesPerFrame = map[Mode]int{
+		Mode3200: 160,
+		Mode2400: 160,
+		Mode1600: 320,
+		Mode1400: 320,
+		Mode1300: 320,
+		Mode1200: 320,
+		Mode700:  320,
+		Mode700B: 320,
+		Mode700C: 320,
+	}
+)
+
+func (mode Mode) BitsPerFrame() int {
+	if n, ok := modeBitsPerFrame[mode]; ok {
+		return n
+	}
+	return 0
+}
+
+func (mode Mode) BytesPerFrame() int {
+	if n, ok := modeBitsPerFrame[mode]; ok {
+		return (n + 7) >> 3
+	}
+	return 0
+}
+
+func (mode Mode) SamplesPerFrame() int {
+	if n, ok := modeSamplesPerFrame[mode]; ok {
+		return n
+	}
+	return 0
+}
+
+func (mode Mode) String() string {
+	if s, ok := modeName[mode]; ok {
+		return s
+	}
+	return "invalid"
+}
+
+// Supported modes
+const (
+	Mode3200 Mode = C.CODEC2_MODE_3200
+	Mode2400 Mode = C.CODEC2_MODE_2400
+	Mode1600 Mode = C.CODEC2_MODE_1600
+	Mode1400 Mode = C.CODEC2_MODE_1400
+	Mode1300 Mode = C.CODEC2_MODE_1300
+	Mode1200 Mode = C.CODEC2_MODE_1200
+	Mode700  Mode = C.CODEC2_MODE_700
+	Mode700B Mode = C.CODEC2_MODE_700B
+	Mode700C Mode = C.CODEC2_MODE_700C
+)
+
+type Codec2 struct {
+	ptr  *C.codec2
+	mode Mode
+}
+
+func New(mode Mode) (*Codec2, error) {
+	if mode < Mode3200 || mode > Mode700C {
+		return nil, errors.New("codec2: unsupported mode")
+	}
+
+	ptr := C.codec2_create(C.int(mode))
+	C.codec2_set_natural_or_gray(ptr, 1)
+
+	return &Codec2{
+		ptr:  ptr,
+		mode: mode,
+	}, nil
+}
+
+func (codec *Codec2) Close() error {
+	if codec.ptr != nil {
+		C.codec2_destroy(codec.ptr)
+		codec.ptr = nil
+	}
+	return nil
+}
+
+func (codec *Codec2) Decode(dst []int16, src []byte) error {
+	var (
+		srcLen    = len(src)
+		srcStep   = codec.mode.BytesPerFrame()
+		srcOffset int
+		dstLen    = len(dst)
+		dstStep   = codec.mode.SamplesPerFrame()
+		dstOffset int
+		minLen    = codec.DecodedLen(srcLen)
+	)
+	if srcLen == 0 && dstLen == 0 {
+		return nil
+	} else if minLen == 0 {
+		return fmt.Errorf("codec2: source buffer size of %d bytes does not align with the block size", srcLen)
+	} else if dstLen < minLen {
+		return fmt.Errorf("codec2: output buffer size of %d is too small to hold %d decoded samples", dstLen, minLen)
+	}
+
+	//log.Printf("decode %d step %d (%d frames) to %d step %d", srcLen, srcStep, srcLen/srcStep, dstLen, dstStep)
+
+	for srcOffset < srcLen {
+		C.codec2_decode(codec.ptr,
+			(*C.short)(unsafe.Pointer(&dst[dstOffset])),
+			(*C.uchar)(unsafe.Pointer(&src[srcOffset])))
+
+		srcOffset += srcStep
+		dstOffset += dstStep
+	}
+
+	return nil
+}
+
+func (codec *Codec2) DecodeBlockSize() int {
+	return codec.mode.BytesPerFrame()
+}
+
+// DecodedLen returns how many samples can be extracted from n bytes of input.
+func (codec *Codec2) DecodedLen(n int) int {
+	var (
+		samples = codec.mode.SamplesPerFrame()
+		bytes   = codec.mode.BytesPerFrame()
+	)
+	if n == 0 || n%bytes != 0 {
+		return 0
+	}
+	return (n * samples) / bytes
+}
+
+func (codec *Codec2) EncodeBlockSize() int {
+	return codec.mode.SamplesPerFrame()
+}
+
+// EncodedLen returns how many bytes are required to encode n samples of input.
+func (codec *Codec2) EncodedLen(n int) int {
+	var (
+		samples = codec.mode.SamplesPerFrame()
+		bytes   = codec.mode.BytesPerFrame()
+	)
+	if n == 0 || n%samples != 0 {
+		return 0
+	}
+	return (n * bytes) / samples
+}
+
+func (codec *Codec2) Encode(dst []byte, src []int16) error {
+	var (
+		srcLen    = len(src)
+		srcStep   = codec.mode.SamplesPerFrame()
+		srcOffset int
+		dstLen    = len(dst)
+		dstStep   = codec.mode.BytesPerFrame()
+		dstOffset int
+		minLen    = codec.EncodedLen(srcLen)
+	)
+	if srcLen == 0 && dstLen == 0 {
+		return nil
+	} else if minLen == 0 {
+		return fmt.Errorf("codec2: source buffer size of %d samples does not align with the block size", srcLen)
+	} else if dstLen < minLen {
+		return fmt.Errorf("codec2: output buffer size of %d is too small to hold %d encoded samples", dstLen, minLen)
+	}
+
+	//log.Printf("encode %d step %d (%d frames) to %d step %d", srcLen, srcStep, srcLen/srcStep, dstLen, dstStep)
+
+	for srcOffset < srcLen {
+		C.codec2_encode(codec.ptr,
+			(*C.uchar)(unsafe.Pointer(&dst[dstOffset])),
+			(*C.short)(unsafe.Pointer(&src[srcOffset])))
+
+		dstOffset += dstStep
+		srcOffset += srcStep
+	}
+
+	return nil
+}
---
+++ nb/codec2/codec2_cgo_test.go
@@ -0,0 +1,105 @@
+// +build cgo
+
+package codec2
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/go-voice/voice/internal/tests"
+)
+
+var testModes = []Mode{Mode3200, Mode1600, Mode1200, Mode700C}
+
+func TestMode(t *testing.T) {
+	for mode := Mode3200; mode <= Mode700C; mode++ {
+		t.Run(mode.String(), func(t *testing.T) {
+			codec, err := New(mode)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer codec.Close()
+
+			if l := codec.DecodeBlockSize(); l == 0 {
+				t.Fatal("decode block size returned 0")
+			} else {
+				t.Logf("decode block size %d", l)
+			}
+			if l := codec.EncodeBlockSize(); l == 0 {
+				t.Fatal("encode block size returned 0")
+			} else {
+				t.Logf("encode block size %d", l)
+			}
+		})
+	}
+}
+
+var testSuites = []string{"female", "male", "violin"}
+
+func TestDecode(t *testing.T) {
+	for _, suite := range testSuites {
+		t.Run(suite, func(t *testing.T) {
+			for _, mode := range testModes {
+				t.Run(mode.String(), func(t *testing.T) {
+					testDecode(t, suite, mode)
+				})
+			}
+		})
+	}
+}
+
+func testDecode(t *testing.T, suite string, mode Mode) {
+	t.Helper()
+
+	codec, err := New(mode)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer codec.Close()
+
+	var (
+		bytes = tests.LoadBytes(t, filepath.Join("testdata", fmt.Sprintf("%s-%s.codec2", suite, mode)))
+		want  = tests.LoadPCMS16(t, filepath.Join("testdata", fmt.Sprintf("%s-%s.decoded.raw", suite, mode)))
+		test  = make([]int16, len(want))
+	)
+	if err = codec.Decode(test, bytes); err != nil {
+		t.Fatal(err)
+	}
+	// tests.ComparePCMS16(t, test, want, 0)
+}
+
+func TestEncode(t *testing.T) {
+	for _, suite := range testSuites {
+		t.Run(suite, func(t *testing.T) {
+			for _, mode := range testModes {
+				t.Run(mode.String(), func(t *testing.T) {
+					testEncode(t, suite, mode)
+				})
+			}
+		})
+	}
+}
+
+func testEncode(t *testing.T, suite string, mode Mode) {
+	t.Helper()
+
+	codec, err := New(mode)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer codec.Close()
+
+	var (
+		samples = tests.LoadPCMS16(t, filepath.Join("..", "testdata", fmt.Sprintf("%s.raw", suite)))
+		want    = tests.LoadBytes(t, filepath.Join("testdata", fmt.Sprintf("%s-%s.codec2", suite, mode)))
+		test    = make([]byte, len(want))
+	)
+	if blockSize := mode.SamplesPerFrame(); len(samples)%blockSize != 0 {
+		samples = samples[:len(samples)-(len(samples)%blockSize)]
+	}
+	if err = codec.Encode(test, samples); err != nil {
+		t.Fatal(err)
+	}
+	tests.CompareBytes(t, test, want, 0)
+}
